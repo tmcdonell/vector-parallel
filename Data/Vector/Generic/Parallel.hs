458cde9b763b4c43e6e3ddccf7e381a5e6daa041
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2011-08-18 11:30:20 +0900

    apply join operation as parallel binary reduction

--- Data/Vector/Generic/Parallel.hs
+++ Data/Vector/Generic/Parallel.hs
@@ -83,13 +83,13 @@ foldMap_ f c z vec = splitjoin_ (G.foldl' (flip (c . f)) z) (foldl' c z) vec
 splitjoin :: (Vector v a, NFData b) => (v a -> b) -> ([b] -> b) -> v a -> b
 splitjoin f g vec
   | numCapabilities == 1 = f vec
-  | otherwise            = g (parSplit f vec)
+  | otherwise            = runPar $ parJoin g =<< parSplit f vec
 
 {-# INLINE [1] splitjoin_ #-}
 splitjoin_ :: Vector v a => (v a -> b) -> ([b] -> b) -> v a -> b
 splitjoin_ f g vec
   | numCapabilities == 1 = f vec
-  | otherwise            = g (parSplit_ f vec)
+  | otherwise            = runPar $ parJoin_ g =<< parSplit_ f vec
 
 {-# RULES
 "split/join"    forall f g p x v. splitjoin  f p (splitjoin  g x v)     = splitjoin  (f . g) p v
@@ -100,14 +100,22 @@ splitjoin_ f g vec
 -- Split a vector into chunks and apply the given operation to each section in
 -- parallel. Return the list of partial results.
 --
-parSplit :: (Vector v a, NFData b) => (v a -> b) -> v a -> [b]
-parSplit f = runPar . parMap f . splitVec
+parSplit :: (Vector v a, NFData b) => (v a -> b) -> v a -> Par [b]
+parSplit f = parMap f . splitVec
+
+parJoin :: NFData b => ([b] -> b) -> [b] -> Par b
+parJoin _ [x] = return x
+parJoin c xs  = parJoin c =<< parMap c (splitList xs)
 
 -- Like 'parSplit', but the function applied to the vector is only head strict,
 -- not fully strict.
 --
-parSplit_ :: Vector v a => (v a -> b) -> v a -> [b]
-parSplit_ f = runPar . parMap_ f . splitVec
+parSplit_ :: Vector v a => (v a -> b) -> v a -> Par [b]
+parSplit_ f = parMap_ f . splitVec
+
+parJoin_ :: ([b] -> b) -> [b] -> Par b
+parJoin_ _ [x] = return x
+parJoin_ c xs  = parJoin_ c =<< parMap_ c (splitList xs)
 
 pval_ :: a -> Par (IVar a)
 pval_ = spawn_ . return
@@ -118,13 +126,16 @@ parMap_ f xs = mapM (pval_ . f) xs >>= mapM get
 -- Split a vector into evenly sized chunks.
 --
 splitVec :: Vector v a => v a -> [v a]
-splitVec vec
-  = snd $ mapAccumL (\v -> swap . flip G.splitAt v) vec cut
+splitVec vec = snd $ mapAccumL (\v -> swap . flip G.splitAt v) vec cut
   where
     len            = G.length vec
     pieces         = auto_partition_factor * numCapabilities
     (step, remain) = len `quotRem` pieces
     cut            = replicate remain (step + 1) ++ replicate (pieces - remain) step
+
+splitList :: [a] -> [[a]]
+splitList [] = []
+splitList xs = case splitAt 2 xs of (a,b) -> a : splitList b
 
 
 -- How many tasks per process should we aim for.  Higher numbers improve load
